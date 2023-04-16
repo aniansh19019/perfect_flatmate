49f06448e2a4d412743b2cdd1678018096042399
Mansi Singhal <mansi19370@iiitd.ac.in>
2023-04-17 02:36:40 +0530

    matches and chat UI

--- lib/pages/matches.dart
+++ lib/pages/matches.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:perfect_flatmate/pages/messaging.dart';
+import 'package:perfect_flatmate/util/theme.dart';
 
 class Matches extends StatefulWidget {
   const Matches({super.key});
@@ -12,8 +14,53 @@ class Matches extends StatefulWidget {
 class _MatchesState extends State<Matches> {
   @override
   Widget build(BuildContext context) {
+    final List<Map<String, dynamic>> arrMatches = [
+      {
+        'title': 'Mansi',
+        'image': 'https://picsum.photos/id/1/200/200',
+      },
+      {
+        'title': 'Shanaya',
+        'image': 'https://picsum.photos/id/2/200/200',
+      },
+      {
+        'title': 'Naira',
+        'image': 'https://picsum.photos/id/3/200/200',
+      },
+    ];
     return Container(
-      child: Text("Matches"),
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text("Matches"),
+        ),
+        body: ListView.separated(
+          itemCount: arrMatches.length,
+          separatorBuilder: (context, index) {
+            return Divider(
+              thickness: 2,
+            );
+          },
+          itemBuilder: (context, index) {
+            return ListTile(
+              leading: CircleAvatar(
+                backgroundImage: NetworkImage(arrMatches[index]['image']),
+              ),
+              title: Text(
+                arrMatches[index]['title'],
+                style: CustomTheme.h1,
+              ),
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => Messaging(),
+                  ),
+                );
+              },
+            );
+          },
+        ),
+      ),
     );
   }
 }
