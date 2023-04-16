54da5966c71f48d624fe8d18719a9f4fc24caf6e
Aniansh Raj <aniansh19019@iiitd.ac.in>
2023-04-16 16:44:48 +0530

    added forms.dart

---
+++ lib/util/theme.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class CustomTheme
+{
+  static TextStyle title = const TextStyle(
+    fontSize: 22,
+    fontWeight: FontWeight.bold,
+
+  );
+  static TextStyle h1 = const TextStyle(
+    fontSize: 20
+    
+  );
+  static TextStyle h2 = const TextStyle(
+    fontSize: 16
+    
+  );
+  static TextStyle body = const TextStyle(
+    fontSize: 12
+  );
+  static TextStyle error = const TextStyle(
+    color: Colors.red
+  );
+}
